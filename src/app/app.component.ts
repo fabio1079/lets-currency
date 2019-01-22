13b8849521f6b01aef9965f4da50569584126f39
Fabio Teixeira <fabio1079@gmail.com>
2019-01-22 12:52:33 -0200

    Use fixer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { FixerService, Query } from "./fixer.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'lets-currency';
+export class AppComponent implements OnInit {
+  result = 0.0;
+  error = false;
+  amount = 1;
+  query: Query = {
+    from: "USD",
+    to: "BRL"
+  };
+
+  constructor(private fixer: FixerService) {}
+
+  ngOnInit() {
+    this.getCurrency();
+  }
+
+  getCurrency() {
+    this.fixer.fetch(this.query).subscribe(({success, rates}) => {
+      if (success) {
+        this.result = this.calcResult(rates[this.query.from], rates[this.query.to], this.amount);
+        this.error = false;
+      } else {
+        this.error = true;
+        this.result = 0;
+      }
+    });
+  }
+
+  private calcResult(from: number, to:number, amount: number) {
+    return (to/from) * amount;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule }    from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
