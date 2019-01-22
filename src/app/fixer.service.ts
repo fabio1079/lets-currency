905da9024fe4d2b98c228fe11cb77c3cc95c99b3
Fabio Teixeira <fabio1079@gmail.com>
2019-01-22 11:59:41 -0200

    Add Fixer service

---
+++ src/app/fixer.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+const BASE_URL = "http://data.fixer.io/api";
+const ACCESS_KEY = "e99d5d81dcd71097fefcba2fb9124bfb";
+
+export type Query = {
+  from: string;
+  to: string;
+};
+
+export interface FixerCall {
+  success: boolean;
+  timestamp: number;
+  base: string;
+  date: string;
+  rates: object;
+}
+
+@Injectable({
+  providedIn: "root"
+})
+export class FixerService {
+  constructor(private http: HttpClient) {}
+
+  private latestUrl({ from, to }: Query): string {
+    return `${BASE_URL}/latest?access_key=${ACCESS_KEY}&symbols=${from},${to}`;
+  }
+
+  fetch(query: Query): Observable<FixerCall> {
+    const URL = this.latestUrl(query);
+
+    return this.http.get<FixerCall>(URL);
+  }
+}
